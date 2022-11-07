d2ee4a761d58027ad72fcef38ae3a18a6c3080f3
antonchuvirau <anton_chuvirau@icloud.com>
2022-11-07 12:32:33 +0100

    Chnages

--- app/js/init.js
+++ app/js/init.js
@@ -1,51 +1,49 @@
 'use strict';
 
 // Constants
-const PAYMENT_METHODS_DATA_WITH_CARD = [
-    {
-        id: 0,
-        name: `erip`,
-        title: `Оплатить картой`,
-        checked: false
-    }
-];
-const PAYMENT_METHODS_DATA = [
-    {
+const PAYMENT_METHODS_DATA_WITH_CARD = [{
+    id: 0,
+    name: `erip`,
+    title: `Оплатить картой`,
+    checked: false
+}];
+const PAYMENT_METHODS_DATA = [{
         id: 0,
         name: `erip`,
         title: `Оплатить через ЕРИП`,
         checked: false
     },
-{
-    id: 4,
-    name: `installment`,
-    title: `Кредит от&nbsp;Альфа&nbsp;банка до&nbsp;12&nbsp;месяцев`,
-    checked: false
-},
-{
-    id: 2,
-    name: `halva`,
-    title: `Рассрочка от 2 до 9 месяцев по карте Халва`,
-    checked: false
-},
-{
-    id: 3,
-    name: `offline`,
-    title: `В отделении банка`,
-    checked: false
-},
-{
-    id: 5,
-    name: `online`,
-    title: `Рассрочка на&nbsp;8&nbsp;месяцев по&nbsp;карте&nbsp;Черепаха`,
-    checked: false
-},
-{
-    id: 1,
-    name: `erip`,
-    title: `Оплатить картой`,
-    checked: false
-}];
+    {
+        id: 4,
+        name: `installment`,
+        title: `Кредит от&nbsp;Альфа&nbsp;банка до&nbsp;12&nbsp;месяцев`,
+        checked: false
+    },
+    {
+        id: 2,
+        name: `halva`,
+        title: `Рассрочка от 2 до 9 месяцев по карте Халва`,
+        checked: false
+    },
+    {
+        id: 3,
+        name: `offline`,
+        title: `В отделении банка`,
+        checked: false
+    },
+    // {
+    //     id: 5,
+    //     name: `online`,
+    //     title: `Рассрочка на&nbsp;8&nbsp;месяцев по&nbsp;карте&nbsp;Черепаха`,
+    //     checked: false
+    // },
+    {
+        id: 1,
+        name: `erip`,
+        title: `Оплатить картой`,
+        checked: false
+    }
+];
 const INSTALLMENT_TERM = 12;
 const INSTALLMENT_RATE = 25.9;
 const INSTALLMENT_TEST_SHOP_ID = `1374`;
